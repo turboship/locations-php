964d631f2b16b18c4976bbf54474ada274300135
james weston <james@savannah.io>
2016-08-07 19:06:06 -0400

    Adding CountryDataUtil

---
+++ src/Utilities/Data/CountryDataUtil.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TurboShip\Locations\Utilities\Data;
+
+class CountryDataUtil
+{
+
+    /**
+     * @return int
+     */
+    public static function getUnitedStatesId()
+    {
+        return 233;
+    }
+    
+}
